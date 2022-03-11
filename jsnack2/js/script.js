62eca1b27e8d90d9427b9329f16749470e21719d
giorgios17 <giosur@live.it>
2022-03-11 13:44:57 +0100

    ex while done

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,8 +1,12 @@
 //Il software deve chiedere per 5 volte all’utente di inserire un numero. 
 //Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
 
+
+
 const numbers = [];
 let somma = 0;
+
+/* VERSIONE FOR
 
 for(let i=0; i < 5; i++){
     numbers.push(parseInt(prompt('Inserisci un numero')))
@@ -11,3 +15,20 @@ for(let i=0; i < 5; i++){
 
 console.log(numbers)
 console.log(somma)
+*/
+
+// VERSIONE WHILE
+
+
+while(numbers.length < 5){
+    numbers.push(parseInt(prompt('Inserisci un numero')))
+}
+
+for(let i=0; i < numbers.length; i++){
+    somma += numbers[i];
+}
+
+console.log(numbers)
+console.log(somma)
+
+
